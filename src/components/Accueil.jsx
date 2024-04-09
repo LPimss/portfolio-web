c533c554740d3b31d74bf8e77bfefe7db0917fd9
Kelian ROULEZ <kelian.roulez@sio.lan>
2024-04-09 17:20:20 +0200

    second commit accueil

--- src/components/Accueil.jsx
+++ src/components/Accueil.jsx
@@ -2,7 +2,54 @@ import React from 'react'
 
 const Accueil = () => {
   return (
-    <div>Accueil</div>
+  <>
+    <div className="flex flex-col items-center justify-center h-1/2 bg-gray-300">
+      <div className="rounded-full w-36 h-36 bg-gray-300 flex items-center justify-center mb-4">
+        <img src="..\img\shrek.png" alt="Image" className="rounded-full w-32 h-32" />
+      </div>
+      <div className="mb-2">Kélian Roulez</div>
+        <div className="mx-52 my-20 text-center">
+          Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+        </div>
+    </div>
+
+    <div className='flex mb-4 my-4 justify-center'>
+
+      <div className='flex flex-col  w-1/2 items-center border-r-8 border-black'>
+        <h2 className='text-xl my-8 font-semibold'>Infos</h2>
+        <p className='text-2xl'>CV</p>
+        <div>
+          Le Lorem Ipsum est simplement Le Lorem Ipsum <br /> est simplement Le Lorem Ipsum est simplement
+        </div>
+      </div>
+
+      <div className='flex flex-col w-1/2 items-center'>
+        <h2 className='text-xl my-8 font-semibold'>Compétences</h2>
+        <div className='flex gap-x-4 my-4'>
+          <p className='text-xl'>LOGO</p>
+          <p className='text-xl'>HTML</p>
+        </div>
+        <div className='flex gap-x-4 my-4'>
+          <p className='text-xl'>LOGO</p>
+          <p className='text-xl'>CSS</p>
+        </div>
+        <div className='flex gap-x-4 my-4'>
+          <p className='text-xl'>LOGO</p>
+          <p className='text-xl'>PHP</p>
+        </div>
+        <div className='flex gap-x-4 my-4'>
+          <p className='text-xl'>LOGO</p>
+          <p className='text-xl'>Javascript</p>
+        </div>
+      </div>
+
+    </div>
+    
+    <footer>
+
+    </footer>
+    
+  </>
   )
 }
 
