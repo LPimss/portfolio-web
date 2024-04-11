ab1b3017ea2c01781e11e0dad813334530a49556
Kélian <kelian.roulez@gmail.com>
2024-04-11 18:11:57 +0200

    last commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <div className='bg-black p-2 fixed bottom-0 left-0 w-full'>
       <ul className='text-white flex items-center justify-center'> 
         <li>
-          <img className='size-10 bg-white' src="..\img\linkedin.png" alt="LinkedIn"></img>
+          <a href='https://www.linkedin.com/in/kelian-roulez-021333250/'><img className='size-10 bg-white' src="/img/linkedin.png" alt="LinkedIn"></img></a>
         </li>
       </ul>
     </div>
